d39be62785aed6e10ce32c055ac2ce7526c30311
Zaid Siddiqui <zaidsiddiqui2401@gmail.com>
2026-01-28 19:37:34 -0500

    changed to helloworld program

--- ExampleFile.java
+++ ExampleFile.java
@@ -1,6 +1,6 @@
-public interface Connectable {
-	public void connect() throws IOException;
-	public void send(String message);	
-	public String receive();
-	public int getPort();
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
+
